0945fa4f5305e9778ebf0f78141416c40fceda5e
kdshapiro10 <111926282+kdshapiro10@users.noreply.github.com>
2025-02-13 16:14:00 -0700

    script.js
    changed file name

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // endpoint here: https://developer.themoviedb.org/reference/movie-popular-list
 function getPopularMovies(){
     // the endpoint
-    let url = "https://api.themoviedb.org/3/movie/popular?api_key=4aa26ebfb227df12f5388e7cbcadcc79language=en-US&page=1";
+    let url = "https://api.themoviedb.org/3/movie/popular?api_key=6103f928103518e6b9b7a0280f0f9a28&language=en-US&page=1";
     // the place on the page where we'll display the movies
     let popularMovies = document.getElementById("popular");
     let imgUrl = "https://image.tmdb.org/t/p/w400";
@@ -10,15 +10,42 @@ function getPopularMovies(){
 
     // ajax time!
     // create the object
-    const data = null;
+    // data = null;
 
     const xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+    //xhr.withCredentials = true;
 
     xhr.addEventListener('readystatechange', function () {
-    if (this.readyState === this.DONE) {
-        console.log(this.responseText);
-    }
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+
+            let json = JSON.parse(this.response);
+
+            let html = "";
+
+            // display featured movie
+            html +=  `<section id="featured">
+                                <h3>${json.results[0].title}</h3>
+                                <img src="${imgUrl}${json.results[0].poster_path}" alt="">
+                                <p>"${json.results[0].overview}"</p>
+                            </section>`;
+
+            for(let i = 1; i < 19; i++){
+                    html += `<section class="movie">
+                                        <img src="${imgUrl}${json.results[i].poster_path}" alt="">
+                                        <div>
+                                            <h3>${json.results[i].title}</h3>
+                                            <p>${json.results[i].overview}
+                                                <span class="vote">Vote Average: ${json.results[i].vote_average}</span>
+                                            </p>
+                                        </div>
+                                    </section>`;
+            }
+
+            //add to page
+            popularMovies.innerHTML = html;
+
+        }
     });
 
     xhr.open('GET', url);
@@ -68,15 +95,17 @@ function getBirthYearMovies(e){
     e.preventDefault();
 
     // Get the user's input/year value
-    // TO DO
+    let year = encodeURI(document.getElementById("userYear").value);
     // the place on the page where we'll add the movies
     let birthYearMovies = document.getElementById("birthYear");
 
     if(year < 1940 || year > 2024 || year == ""){
-        birthYearMovies.innerHTML = `<p style="color: red; background-color: white;">Please enter a year between 1940 and 2022</p>`;
+        birthYearMovies.innerHTML = `<p style="color: red; background-color: white;">Please enter a year between 1940 and 2025</p>`;
     }else{
         // TO DO - Build the endpoint we need (this one has additional parameters)
-        // TO DO
+        let beginURL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=revenue.desc";
+        let endURL = "&sort_by=revenue.desc";
+        let url = `${}`
         let imgUrl = "https://image.tmdb.org/t/p/w400";
 
         // ajax time!
